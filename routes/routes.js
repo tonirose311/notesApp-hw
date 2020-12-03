49c4d13746bbcc51b284c8741740e3bbc5f93b8f
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-02 20:03:29 -0600

    debug routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+
 module.exports = (app) => {
   app.post("/api/notes", function (req, res) {
-    let createdNote = req.body; //Need to Id to create note.
+    let createdNote = req.body;
     createdNote.id = uuidv4();
-    // console.log("uuid", uuidv4());
+
     console.log(createdNote);
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
       if (err) throw err;
       var noteTaker = JSON.parse(data);
       noteTaker.push(createdNote);
@@ -16,31 +17,30 @@ module.exports = (app) => {
       return console.log("Added new note: " + createdNote.title);
     });
     function updateDb(noteTaker) {
-      fs.writeFile("db/db.json", JSON.stringify(noteTaker, "\t"), (err) => {
-        //Update the json file
+      fs.writeFile("./Develop/db/db.json", JSON.stringify(noteTaker, "\t"), (err) => {
         if (err) throw err;
         return true;
       });
     }
   });
   app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
       if (err) throw err;
       var noteTaker = JSON.parse(data);
       res.json(noteTaker);
     });
   });
   app.get("/api/notes/:id", function (req, res) {
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
       if (err) throw err;
       var noteTaker = JSON.parse(data);
       res.json(noteTaker);
     });
-    // res.json(noteTaker[req.params.id]);
+
   });
   app.delete("/api/notes/:id", function (req, res) {
     console.log(req.params);
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
       if (err) throw err;
       var noteTaker = JSON.parse(data);
       res.json(noteTaker);
@@ -50,11 +50,9 @@ module.exports = (app) => {
           status: "error",
           error: "req body cannot be empty",
         });
-        // If there is an Id, then remove that item from the Json file.
       }
     });
-    // noteTaker(req.params.id);
-    // updateDb();
+
     console.log("Deleted note with id " + req.params.id);
   });
   app.get("/notes", function (req, res) {
