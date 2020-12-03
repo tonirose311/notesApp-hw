1e9d93cf5cc193c7c9506bf7bc04a8fe6bda282f
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-02 19:58:42 -0600

    added dependencies

--- routes/routes.js
+++ routes/routes.js
@@ -1,52 +1,66 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = app => {
-
-    fs.readFile("../notesApp-hw/db/db.json","utf8", (err, data) => {
-
-        if (err) throw err;
-
-        var notes = JSON.parse(data);
-
-        
-        app.get("/api/notes", function(req, res) {
-            res.json(notes);
-        });
-
-        app.post("/api/notes", function(req, res) {
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDb();
-            return console.log("Added new note: "+newNote.title);
-        });
-
-        
-        app.get("/api/notes/:id", function(req,res) {
-            res.json(notes[req.params.id]);
-        });
-
-        app.delete("/api/notes/:id", function(req, res) {
-            notes.splice(req.params.id, 1);
-            updateDb();
-            console.log("Deleted note with id "+req.params.id);
-        });
-
-        app.get('/notes', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/notes.html"));
-        });
-        
-        app.get('*', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
-        });
-
-        function updateDb() {
-            fs.writeFile("../notesApp-hw/db/db.json",JSON.stringify(notes,'\t'),err => {
-                if (err) throw err;
-                return true;
-            });
-        }
-
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+module.exports = (app) => {
+  app.post("/api/notes", function (req, res) {
+    let createdNote = req.body; //Need to Id to create note.
+    createdNote.id = uuidv4();
+    // console.log("uuid", uuidv4());
+    console.log(createdNote);
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      noteTaker.push(createdNote);
+      updateDb(noteTaker);
+      res.redirect("/notes");
+      return console.log("Added new note: " + createdNote.title);
     });
-
-}
+    function updateDb(noteTaker) {
+      fs.writeFile("db/db.json", JSON.stringify(noteTaker, "\t"), (err) => {
+        //Update the json file
+        if (err) throw err;
+        return true;
+      });
+    }
+  });
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      res.json(noteTaker);
+    });
+  });
+  app.get("/api/notes/:id", function (req, res) {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      res.json(noteTaker);
+    });
+    // res.json(noteTaker[req.params.id]);
+  });
+  app.delete("/api/notes/:id", function (req, res) {
+    console.log(req.params);
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      var noteTaker = JSON.parse(data);
+      res.json(noteTaker);
+      noteTaker.splice(req.params.id);
+      if (!req.params.id) {
+        return res.status(400).json({
+          status: "error",
+          error: "req body cannot be empty",
+        });
+        // If there is an Id, then remove that item from the Json file.
+      }
+    });
+    // noteTaker(req.params.id);
+    // updateDb();
+    console.log("Deleted note with id " + req.params.id);
+  });
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
